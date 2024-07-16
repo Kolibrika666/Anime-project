f71ae30ba7b462ded4c6de9a703a138e7623a8b9
Kolibrika666 <kolibrika666@gmail.com>
2024-07-16 13:52:28 +0700

    init: buttonBefore

---
+++ src/Layout/Pages/RandomAnime/RandomAnime.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RandomAnime = () => {
+    return (
+        <div>
+            bilgbiv
+        </div>
+    );
+};
+
+export default RandomAnime;
---
+++ src/Layout/Pages/StartPage/StartPage.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useEffect } from 'react';
+import s from './StartPage.module.scss'
+import { useAppDispatch, useAppSelector } from '../../../store';
+import { getFavoriteAnime } from '../../../api/catalogAnimeApi';
+import { setAnime, setCatalogAnime } from '../../../store/animeCatalog/animeCardSlice';
+import AnimeCard from '../CatalogAnime/AnimeCard';
+
+const StartPage = () => {
+
+    const anime = useAppSelector(store => store.animeCard.anime)
+    const dispatch = useAppDispatch()
+
+    const getfavoriteAnime = useCallback(async () => {
+        const data = await getFavoriteAnime()
+        dispatch(setAnime(data))
+    }, [dispatch])
+
+    useEffect(() => {
+        getfavoriteAnime()
+    }, [getfavoriteAnime])
+
+    return (
+        <div className={s.start}>
+            <img className={s.banner}  src = 'https://media.kitsu.io/anime/cover_images/50/large.jpg' alt = ""/>
+            <article>
+            
+                <h3>Это аниме нравится большинству!</h3>
+            
+            <AnimeCard canonicalTitle = {anime.attributes.canonicalTitle}
+            posterImage={anime.attributes.posterImage}
+            status={anime.attributes.status}
+            averageRating={anime.attributes.averageRating}/>
+            <div className={s.block}>
+            <h4>Описание</h4>
+            <p className={s.synopsis}>{anime.attributes.synopsis}</p>
+            </div>
+            </article>
+
+        </div>
+    );
+};
+
+export default StartPage;
